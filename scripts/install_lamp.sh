cd51c207b34bf49180150130fd7730eb897d8656
fedora Cloud User <fedora@fedora-practica03.openstacklocal>
2023-10-23 18:45:09 +0000

    install_lamp.sh

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+#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
 set -x
 sudo dnf update
 
 sudo dnf install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
+
+sudo dnf install community-mysql-server -y
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+sudo dnf install php -y
+sudo dnf install php-mysqlnd -y
+sudo systemctl restart httpd
